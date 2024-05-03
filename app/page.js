f99041a07eb4f2ab7ef5ab6a1b63802c54ca0bc9
SmitBhamwala <smitbhamwala@gmail.com>
2024-05-03 19:21:42 +0530

    Modified meal loader css

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
         <div>
           <div className={classes.hero}>
             {/* <h1>NextLevel Food for NextLevel Foodies</h1> */}
-            <h1>Kachraseth's nextlevel foodzone for Foodies</h1>
+            <h1>Kachraseth&apos;s nextlevel foodzone for Foodies</h1>
             <p>Taste and share food from all over the world.</p>
           </div>
           <div className={classes.cta}>
